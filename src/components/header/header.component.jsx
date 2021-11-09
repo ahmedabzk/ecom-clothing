f0624683025eb3e55ccb0743ca0a823f5bb271f2
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-09 12:52:12 +0300

    --- Auto-Git Commit ---

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({currentUser, }) => (
+const Header = ({currentUser, hidden}) => (
     <div className="header">
     <Link className="logo-container" to='/'>
     <Logo className="logo"/>
@@ -35,7 +35,11 @@ const Header = ({currentUser, }) => (
     )}
 
     <CartIcon/>
-    <CartDropdown/>
+
+    {
+        hidden ? null : <CartDropdown/>
+    }
+    
 
     </div>
 
