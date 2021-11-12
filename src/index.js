a61787a26555d2e1cdab6f63e82e3d4f8245eff5
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-12 09:52:11 +0300

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import { PersistGate} from 'redux-persist/integration/react';
-
+// redux persist using reselect lib
 
 import {store, persistor} from './redux/store';
 
