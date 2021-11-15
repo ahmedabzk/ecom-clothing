978f943b0b1de026d26bb4ee689722608902a711
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-15 19:58:31 +0300

    --- Auto-Git Commit ---

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -6,10 +6,10 @@ import CollectionPreview from '../collection-preview/collection-preview.componen
 
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
-import './collections-overview.styles.scss';
+import './collection-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-<div className='collections-overview'>
+<div className='collection-overview'>
 {collections.map(({ id, ...otherCollectionProps }) => (
     <CollectionPreview key={id} {...otherCollectionProps} />
 ))}
