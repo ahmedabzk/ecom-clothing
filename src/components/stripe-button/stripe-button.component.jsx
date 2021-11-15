67263ce1027a211d9866dbd43cef3ac76feeb8fa
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-15 21:31:52 +0300

    --- Auto-Git Commit ---

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,5 +3,27 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
-    const 
-}
+    const publishableKey = 'pk_test_51Jw6yjB1BxL0aHkK1OZaNdqdPWyc7DkwezQ5MY7rLGfmIWNA5rBzcNa27mMrN7eRFfFvGB2hnRHZP9aSn7vP8ogy00VtZuxdQK';
+    
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+    return (
+        <StripeCheckout
+            label= 'Pay Now'
+            name= 'ecom-clothing ltd.'
+            billingAddress
+            shippingAddress
+            image= 'https://svgshare.com/i/Cuz.svg'
+            description= {`Your total is $${price}`}
+            amount= {priceForStripe}
+            panelLabel= 'Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+
+        />
+    );
+};
+
+export default StripeCheckoutButton;
