62d1fc87e3ee309ca25091398540ab35b043c391
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-16 15:17:07 +0300

    --- Auto-Git Commit ---

---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,11 @@
+import styles from 'styled-components';
+
+const HomePageContainer = styles.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 80px;
+
+`;
+
+export default HomePageContainer;
