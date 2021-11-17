ee3779a5176ca463645878066e015f1aa601a7f8
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-17 15:20:47 +0300

    --- Auto-Git Commit ---

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'production') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
