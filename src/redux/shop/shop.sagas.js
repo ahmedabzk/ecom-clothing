6762efb8cfb25ed2822f544086eb487ce47d6b92
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-18 20:19:30 +0300

    --- Auto-Git Commit ---

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -2,6 +2,8 @@ import { takeEvery } from 'redux-saga/effects';
 
 import ShopActionTypes from './shop.types';
 
+import { firestore, convertCollectionsSnapshotToMap } from '../../'
+
 export function* fetchCollectionsAsync() {
     yield console.log('I am fired');
 
