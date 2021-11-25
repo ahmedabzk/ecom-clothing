ba83675c2928c4cd18404076b3dcc6d19532dfc6
ahmedabzk <ahmed.abzk@gmail.com>
2021-11-25 18:12:38 +0300

    updated checkout component with useselector hook

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { connect} from 'react-redux';
-import { createStructuredSelector} from 'reselect';
+import { useSelector } from 'react-redux';
+
 
 import { selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total}) => (
+const CheckoutPage = () => {
+  const cartItems = useSelector(selectCartItems);
+  const total = useSelector(selectCartTotal);
+    return (
     <div className="checkout-page">
     <div className="checkout-header">
     <div className="header-block">
@@ -45,10 +48,6 @@ const CheckoutPage = ({ cartItems, total}) => (
       <StripeCheckoutButton price={total}/>
     </div>
 );
+}
 
-const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    total: selectCartTotal
-})
-
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
